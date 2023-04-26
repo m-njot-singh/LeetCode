db8e1acd8fb9c81b4f70fe5fe585d0e712dece13
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-04-26 16:56:03 +0530

    Time: 16 ms (8.61%), Space: 42.5 MB (7.95%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> printVertically(String s) {
+        List<String> ans=new ArrayList<>();
+        int m=Integer.MIN_VALUE;
+        String [] temp=s.split(" ");
+        for(String a:temp){
+           m=Math.max(m,a.length());
+        }
+        char[][] matrix=new char[temp.length][m];
+        for(char[] w:matrix){
+            Arrays.fill(w,'0');
+        }
+        for(int i=0;i<temp.length;i++){
+            for(int j=0;j<temp[i].length();j++){
+                // System.out.print(temp[i].charAt(j)+" ");
+                // if(temp[i].charAt(j)){
+                     matrix[i][j]=temp[i].charAt(j);
+                // }
+               
+            }
+            // System.out.println();
+        }
+        for(int j=0;j<matrix[0].length;j++){
+            String q="";
+            String y="";
+            int count=0;
+            for(int i=0;i<matrix.length;i++){
+                if(matrix[i][j]!='0'){
+                    System.out.print(matrix[i][j]+" ");
+                    q=q+y+matrix[i][j];
+                    y="";
+                }
+                else{
+                    y+=" ";
+                }
+            }
+            System.out.println();
+            ans.add(q);
+        }
+        return ans;
+    }
+}
