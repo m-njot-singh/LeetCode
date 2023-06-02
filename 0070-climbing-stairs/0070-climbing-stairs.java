abcb2ca783c697da5cd29aa619edcfd09e9d7acf
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-02 22:58:23 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (33.74%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,23 @@
 class Solution {
-    public static int dfs(int n,int[] dp){
-        if(n==0 || n==1)return 1;
-        if(dp[n]!=-1)return dp[n];
-        int left=dfs(n-1,dp);
-        int right=dfs(n-2,dp);
-        return dp[n]=left+right;
-    }
+    // public static int dfs(int n,int[] dp){
+    //     if(n==0 || n==1)return 1;
+    //     if(dp[n]!=-1)return dp[n];
+    //     int left=dfs(n-1,dp);
+    //     int right=dfs(n-2,dp);
+    //     return dp[n]=left+right;
+    // }
     public int climbStairs(int n) {
         int[] dp=new int[n+1];
-        for(int i=0;i<=n;i++){
-            dp[i]=-1;
+        dp[0]=1;
+        dp[1]=1;
+        // for(int i=0;i<=n;i++){
+        //     dp[i]=-1;
+        // }
+        // return dfs(n,dp);
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return dfs(n,dp);
+        return dp[n];
+        
     }
 }
