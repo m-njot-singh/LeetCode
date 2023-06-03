e7d644167021d2e519bef0a99ef8738ef8941a3c
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-04 01:19:23 +0530

    Time: 3 ms (29.51%), Space: 44.4 MB (77.57%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,23 +1,39 @@
 class Solution {
-    public static int help(int[][]grid,int start,int end,int n, int m,int[][] dp){
-        if(start>n || end>m)return (int)1e9;
-        if(n==start && m==end)return grid[n][m];
-        if(dp[start][end]!=-1) return dp[start][end];
-        int rightsum=grid[start][end]+help(grid,start,end+1,n,m,dp);
-        int downsum=grid[start][end]+help(grid,start+1,end,n,m,dp);
+//     public static int help(int[][]grid,int start,int end,int n, int m,int[][] dp){
+//         if(start>n || end>m)return (int)1e9;
+//         if(n==start && m==end)return grid[n][m];
+//         if(dp[start][end]!=-1) return dp[start][end];
+//         int rightsum=grid[start][end]+help(grid,start,end+1,n,m,dp);
+//         int downsum=grid[start][end]+help(grid,start+1,end,n,m,dp);
         
-        return dp[start][end]=Math.min(rightsum,downsum);
+//         return dp[start][end]=Math.min(rightsum,downsum);
         
         
-    }
+//     }
     public int minPathSum(int[][] grid) {
         int [][] dp= new int [grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                dp[i][j]=-1;
+                if(i==0 && j==0)dp[i][j]=grid[i][j];
+                else{
+                    int right=grid[i][j];
+                    if(j>0){
+                        right+=dp[i][j-1];
+                    }
+                    else{
+                        right+=1e9;
+                    }
+                    int down=grid[i][j];
+                    if(i>0){
+                        down+=dp[i-1][j];
+                    }
+                    else{
+                        down+= 1e9;
+                    }
+                    dp[i][j]=Math.min(down,right);
+                }
             }
         }
-        int ans=help(grid,0,0,grid.length-1,grid[0].length-1,dp);
-        return ans;
+        return dp[grid.length-1][grid[0].length-1];
     }
 }
