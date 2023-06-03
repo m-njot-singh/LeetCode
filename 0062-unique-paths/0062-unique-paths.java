488ed1295482f37ec7cc81e19847b284c036a6c1
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-04 00:07:32 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (43.37%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,33 +1,27 @@
 class Solution {
-    // public static int path(int row,int col,int n, int m,int[][] adj,int[][] dp){
-    //     if(row<0 || col<0 || row>n || col>m )return 0;
-    //     if(row==n && col==m)return 1;
-    //     if(dp[row][col]!=-1)return dp[row][col];
-    //     int down=path(row+1,col,n,m,adj,dp);
-    //     int right=path(row,col+1,n,m,adj,dp);
-    //     return dp[row][col]=down+right;
-    // }
+   
     public int uniquePaths(int n, int m) {
-        int [][] dp=new int [n][m];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++){
+        int [] prev=new int [n];
+        for(int i=0;i<m;i++){
+            int[] temp=new int[n];
+            for(int j=0;j<n;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    temp[j]=1;
                     continue;
                 }
                 int down=0;
                 if(i>0){
-                    down=dp[i-1][j];
+                    down=prev[j];
                 } 
                 int right=0;
                 if(j>0){
-                    right=dp[i][j-1];
+                    right=temp[j-1];
                 }
-                dp[i][j]=down+right;
+                temp[j]=down+right;
             }
-            
+            prev=temp;
         }
-        return dp[n-1][m-1];
+        return prev[n-1];
         
     }
 }
