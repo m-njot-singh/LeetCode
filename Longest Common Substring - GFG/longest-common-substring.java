ba98eb3d570c863f4305f3faa32c1118fc9c2f2b
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-06 22:26:24 +0530

    Added solution - LeetHub

---
+++ Longest Common Substring - GFG/longest-common-substring.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+            
+            String S1 = read.readLine().trim();
+            String S2 = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int longestCommonSubstr(String s1, String s2, int n, int m){
+
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i] = 0;
+        }
+        int ans=0;
+        for(int ind1=1;ind1<=n;ind1++){
+            for(int ind2=1;ind2<=m;ind2++){
+                if(s1.charAt(ind1-1)==s2.charAt(ind2-1)){
+                    dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+                    ans=Math.max(ans,dp[ind1][ind2]);
+                }
+                    
+                else
+                    dp[ind1][ind2] = 0 ;
+            }
+        }
+        return ans;
+    }
+}
