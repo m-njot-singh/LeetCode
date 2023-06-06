b02f2e61d4f5923b4ae5853daf8a2d3534daf495
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-06 14:20:03 +0530

    Time: 21 ms (46.13%), Space: 44.1 MB (11.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,25 +1,39 @@
 class Solution {
-    public static int dfs(int ind,int target,int[] coins,int[][] dp){
-        if(ind==0){
-            if(target%coins[ind]==0)return target/coins[ind];
-            return (int)1e9;
-        }
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        int not_pick=0+dfs(ind-1,target,coins,dp);
-        int pick=Integer.MAX_VALUE;
-        if(coins[ind]<=target){
-            pick=1+dfs(ind,target-coins[ind],coins,dp);
-        }
-        return dp[ind][target]=Math.min(not_pick,pick);
-    }
-    public int coinChange(int[] coins, int amount) {
-        int n=coins.length;
-        int[][] dp=new int[n][amount+1];
-            for(int[] i:dp){
-                Arrays.fill(i,-1);
+    // public static int dfs(int ind,int target,int[] coins,int[][] dp){
+    //     if(ind==0){
+    //         if(target%coins[ind]==0)return target/coins[ind];
+    //         return (int)1e9;
+    //     }
+    //     if(dp[ind][target]!=-1)return dp[ind][target];
+    //     int not_pick=0+dfs(ind-1,target,coins,dp);
+    //     int pick=Integer.MAX_VALUE;
+    //     if(coins[ind]<=target){
+    //         pick=1+dfs(ind,target-coins[ind],coins,dp);
+    //     }
+    //     return dp[ind][target]=Math.min(not_pick,pick);
+    // }
+    public int coinChange(int[] nums, int target) {
+        int n=nums.length;
+        int[][] dp=new int[n][target+1];
+            for(int i=0;i<=target;i++){
+                if(i%nums[0]==0){
+                    dp[0][i]=i/nums[0];
+                }
+                else{
+                    dp[0][i]=(int)1e9;
+                }
             }
-        int ans=dfs(n-1,amount,coins,dp);
-        if(ans==(int)1e9)return -1;
-        return ans;
+        for(int ind=1;ind<n;ind++){
+            for(int t=0;t<=target;t++){
+                int not_pick=0+dp[ind-1][t];
+        int pick=Integer.MAX_VALUE;
+        if(nums[ind]<=t){
+            pick=1+dp[ind][t-nums[ind]];
+        }
+        dp[ind][t]=Math.min(not_pick,pick);
+            }
+        }
+        if(dp[n-1][target]==(int)1e9)return -1;
+        return dp[n-1][target];
     }
 }
