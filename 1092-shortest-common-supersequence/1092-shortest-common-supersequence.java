a9450bee980b835a0714215dbd349f7371726c84
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-07 11:01:07 +0530

    Time: 15 ms (94.97%), Space: 44.9 MB (83.42%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -13,12 +13,7 @@ class Solution {
                 else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        // for(int i=0;i<=n;i++){
-        //     for(int j=0;j<=m;j++){
-        //         System.out.print(dp[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
+       
         StringBuilder ans=new StringBuilder();
         int i=n;
         int j=m;
