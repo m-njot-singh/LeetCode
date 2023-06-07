14487fac05f8af543eb6d8c919a15638c8db8709
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-07 11:29:32 +0530

    Time: 21 ms (59.52%), Space: 46.8 MB (82.09%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,11 +1,4 @@
 class Solution {
-    public static int dfs(String s,String a,int i,int j,int[][] dp){
-        if(j==0)return 1;
-        if(i==0)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s.charAt(i-1)==a.charAt(j-1))return dp[i][j]=dfs(s,a,i-1,j-1,dp)+dfs(s,a,i-1,j,dp);
-        return dp[i][j]=dfs(s,a,i-1,j,dp);
-    }
     public int numDistinct(String s, String t) {
         int n=s.length();
         int m=t.length();
