ccb069cf6d7b8d89a20f2223786a3da6f732d7bc
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-07 12:11:52 +0530

    Time: 7 ms (30.67%), Space: 43.4 MB (38.24%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,18 +1,19 @@
 class Solution {
-    public static int dfs(String s,String a,int i,int j,int[][] dp){
-        if(i<0)return j+1;
-        if(j<0)return i+1;
-        if(dp[i][j]!=-1)return dp[i][j];
-        if(s.charAt(i)==a.charAt(j))return dp[i][j]=0+dfs(s,a,i-1,j-1,dp);
-        return dp[i][j]=Math.min(1+dfs(s,a,i-1,j,dp),Math.min(1+dfs(s,a,i,j-1,dp),1+dfs(s,a,i-1,j-1,dp)));
-    }
     public int minDistance(String s, String a) {
         int n=s.length();
         int m=a.length();
-        int[][] dp=new int[n][m];
-        for(int[] i:dp){
-            Arrays.fill(i,-1);
+        int[][] dp=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)dp[i][0]=i;
+        for(int j=0;j<=m;j++)dp[0][j]=j;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(s.charAt(i-1)==a.charAt(j-1)) dp[i][j]=0+dp[i-1][j-1];
+                else dp[i][j]=Math.min(1+dp[i-1][j],Math.min(1+dp[i][j-1],1+dp[i-1][j-1]));
+                
+            }
         }
-        return dfs(s,a,n-1,m-1,dp);
+        return dp[n][m];
     }
 }
