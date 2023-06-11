1c5577997650c052b3b95b00953a5b23b9513dc3
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-11 23:16:41 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -29,23 +29,32 @@ class GFG
 //User function Template for Java
 
 class Solution{
-        public static int dfs(int i,int j,int n,int[] arr,int[][] dp){
-        if(i==j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int min=Integer.MAX_VALUE;
-        for(int k=i;k<j;k++){
-            int steps=arr[i-1]*arr[k]*arr[j]+dfs(i,k,n,arr,dp)+dfs(k+1,j,n,arr,dp);
-            min=Math.min(min,steps);
-        }
-        return  dp[i][j]=min;
-    }
+    //     public static int dfs(int i,int j,int n,int[] arr,int[][] dp){
+    //     if(i==j)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int min=Integer.MAX_VALUE;
+    //     for(int k=i;k<j;k++){
+    //         int steps=arr[i-1]*arr[k]*arr[j]+dfs(i,k,n,arr,dp)+dfs(k+1,j,n,arr,dp);
+    //         min=Math.min(min,steps);
+    //     }
+    //     return  dp[i][j]=min;
+    // }
     static int matrixMultiplication(int N, int arr[])
     {
         // code here
         int[][] dp=new int[N][N];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+        for(int i=1;i<N;i++)dp[i][i]=0;
+        for(int i=N-1;i>=1;i--){
+            for(int j=i+1;j<N;j++){
+                int min=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int steps=arr[i-1]*arr[k]*arr[j]+dp[i][k]+dp[k+1][j];
+                    min=Math.min(min,steps);
+                }
+                dp[i][j]=min;
+            }
         }
-        return dfs(1,N-1,N,arr,dp);
+        
+        return dp[1][N-1];
     }
 }
