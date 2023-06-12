b240f53252a0fb23e907440f06e192503e96ec25
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-12 18:22:11 +0530

    Time: 42 ms (85.90%), Space: 41.7 MB (84.10%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,22 +1,30 @@
 class Solution {
     
-    public static int dfs(int i,int j,int[] nums,int[][] dp){
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int max=Integer.MIN_VALUE;
-        for(int k=i;k<=j;k++){
-            int cost=(nums[i-1]*nums[k]*nums[j+1])+dfs(i,k-1,nums,dp)+dfs(k+1,j,nums,dp);
-            max=Math.max(max,cost);
-        }
-        return dp[i][j]=max;
-    }
+    // public static int dfs(int i,int j,int[] nums,int[][] dp){
+    //     if(i>j)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int max=Integer.MIN_VALUE;
+    //     for(int k=i;k<=j;k++){
+    //         int cost=(nums[i-1]*nums[k]*nums[j+1])+dfs(i,k-1,nums,dp)+dfs(k+1,j,nums,dp);
+    //         max=Math.max(max,cost);
+    //     }
+    //     return dp[i][j]=max;
+    // }
     
     public static int max(int n,int[] nums){
         int[][] dp=new int[n+2][n+2];
-        for(int[] i:dp){
-            Arrays.fill(i,-1);
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j)continue;
+                int max=Integer.MIN_VALUE;
+                for(int k=i;k<=j;k++){
+                    int cost=(nums[i-1]*nums[k]*nums[j+1])+dp[i][k-1]+dp[k+1][j];
+                    max=Math.max(max,cost);
+                }
+                dp[i][j]=max;
+            }
         }
-        return dfs(1,n,nums,dp);
+        return dp[1][n];
     }
     
     
