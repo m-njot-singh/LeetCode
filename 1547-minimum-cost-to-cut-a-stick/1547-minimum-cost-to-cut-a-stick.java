58e352d95fce17b1f3db614a42cb23239f701a16
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-12 17:45:02 +0530

    Time: 14 ms (66.80%), Space: 41.3 MB (86.67%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,14 +1,32 @@
 class Solution {
-    public static int dfs(int i,int j,int[] cuts,int[][] dp){
-        if(i>j)return 0;
-        if(dp[i][j]!=-1)return dp[i][j];
-        int min=Integer.MAX_VALUE;
-        for(int k=i;k<=j;k++){
-            int cut=(cuts[j+1]-cuts[i-1])+dfs(i,k-1,cuts,dp)+dfs(k+1,j,cuts,dp);
-            min=Math.min(min,cut);
+    // public static int dfs(int i,int j,int[] cuts,int[][] dp){
+    //     if(i>j)return 0;
+    //     if(dp[i][j]!=-1)return dp[i][j];
+    //     int min=Integer.MAX_VALUE;
+    //     for(int k=i;k<=j;k++){
+    //         int cut=(cuts[j+1]-cuts[i-1])+dfs(i,k-1,cuts,dp)+dfs(k+1,j,cuts,dp);
+    //         min=Math.min(min,cut);
+    //     }
+    //     return dp[i][j]=min;
+    // }
+    
+    public static int mincost(int n,int c,int[]newcuts){
+        int[][] dp=new int[c+2][c+2];
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j)continue;
+                int min=Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++){
+                    int cut=(newcuts[j+1]-newcuts[i-1])+dp[i][k-1]+dp[k+1][j];
+                    min=Math.min(min,cut);
+                }
+                dp[i][j]=min;
+            }
         }
-        return dp[i][j]=min;
+        return dp[1][c];
     }
+    
+    
     public int minCost(int n, int[] cuts) {
         List<Integer> l=new ArrayList<>();
         for(int i=0;i<cuts.length;i++){
@@ -22,10 +40,8 @@ class Solution {
             newcuts[i]=l.get(i);
         }
         Arrays.sort(newcuts);
-        int[][] dp=new int[newcuts.length][newcuts.length];
-        for(int[] i:dp){
-            Arrays.fill(i,-1);
-        }
-        return dfs(1,cuts.length,newcuts,dp);
+        
+        return mincost(n,cuts.length,newcuts);
+        
     }
 }
