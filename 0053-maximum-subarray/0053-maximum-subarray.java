1a05a89eceb81434cf205e8c140598489a52b28a
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-15 14:51:36 +0530

    Time: 1 ms (100.00%), Space: 59.8 MB (13.09%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,6 +3,7 @@ class Solution {
         int ans=nums[0];
         int sum=nums[0];
         for(int i=1;i<nums.length;i++){
+            if(sum<0)sum=0;
            sum=Math.max(nums[i],sum+nums[i]);
             ans=Math.max(ans,sum);
             
