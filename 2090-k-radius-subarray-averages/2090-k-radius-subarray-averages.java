f7a04a156b72d7584d9d38a1f8a191ae2dce82fb
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-06-20 15:13:36 +0530

    Time: 12 ms (48.87%), Space: 60.1 MB (76.18%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,15 +1,9 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
         int[] ans=new int[nums.length];
-        if(k>nums.length){
             Arrays.fill(ans,-1);
-                return ans;
-        }
-        int ind=0;
-        for(int i=0;i<k;i++){
-            ans[i]=-1;
-            ind++;
-        }
+        if(k>nums.length)return ans;
+        int ind=k;
         int target=(k*2)+1;
         int i=0;
         int j=0;
@@ -30,10 +24,6 @@ class Solution {
             
         }
         
-        while(ind<nums.length){
-            ans[ind]=-1;
-            ind++;
-        }
         return ans;
     }
 }
