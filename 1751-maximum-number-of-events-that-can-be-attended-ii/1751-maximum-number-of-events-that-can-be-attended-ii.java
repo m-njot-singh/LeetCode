e85d1db9054a39a2458b34c21d495b70dae993d7
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-07-16 01:04:00 +0530

    Time: 64 ms (58.33%), Space: 93.2 MB (57.41%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.java
@@ -1,15 +1,30 @@
 class Solution {
+    public static int binary(int[][] events,int i){
+        int l=i+1;  int h=events.length-1 ; int check=events[i][1];
+        int ans=Integer.MAX_VALUE;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(events[mid][0]>check){
+                ans=mid;
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return ans;
+    }
     public static int dfs(int i,int n,int k,int[][] events,int[][] dp){
-        int j=0;
         if(i>=n || k==0){
             return 0; 
         }
         if(dp[i][k]!=-1)return dp[i][k];
-        for(j=i+1;j<n;j++){
-            if(events[j][0]>events[i][1]){
-                break;
-            }
-        }
+        // for(j=i+1;j<n;j++){
+        //     if(events[j][0]>events[i][1]){
+        //         break;
+        //     }
+        // }
+        int j=binary(events,i);
         int pick=events[i][2]+dfs(j,n,k-1,events,dp);
         int notpick=dfs(i+1,n,k,events,dp);
         return dp[i][k]=Math.max(pick,notpick);
