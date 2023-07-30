ce3e28e1a7ab1446b113312ec3fea12e3efcafce
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-07-30 16:29:47 +0530

    Time: 12 ms (99.41%), Space: 13.5 MB (75.15%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,54 +1,52 @@
 class Solution {
 public:
-//     bool dfs(int src,vector<bool> &vis,vector<bool> &stack,vector<int> adj[])
-//     {
-//         vis[src]=1;
-//         stack[src]=1;
-//         for(auto x:adj[src])
-//         {
-//             if(!vis[x])
-//             {
-//                 if(dfs(x,vis,stack,adj))
-//                 {
-//                     return true;
-//                 }
-//             }
-//             else
-//             {
-//                 if(stack[x])
-//                 {
-//                     return true;
-//                 }
-//             }
-//         }
-//         stack[src]=0;
-//         return false;
-//     }
-//     bool canFinish(int V, vector<vector<int>>& prerequisites) {
-//         vector<int> adj[V];
-//         int n=prerequisites.size();
-//         int m=prerequisites[0].size();
-//         for(int i=0;i<n;i++)
-//         {
-//             adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
-//         }
-//         vector<bool> vis(V,0);
-//         vector<bool> stack(V,0);
+    bool dfs(int src,vector<bool> &vis,vector<bool> &stack,vector<int> adj[])
+    {
+        vis[src]=1;
+        stack[src]=1;
+        for(auto x:adj[src])
+        {
+            if(!vis[x])
+            {
+                if(dfs(x,vis,stack,adj))
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if(stack[x])
+                {
+                    return true;
+                }
+            }
+        }
+        stack[src]=0;
+        return false;
+    }
+    bool canFinish(int V, vector<vector<int>>& prerequisites) {
+        vector<int> adj[V];
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+        }
+        vector<bool> vis(V,0);
+        vector<bool> stack(V,0);
         
         
-//         for(int i=0;i<V;i++)
-//         {
-//             if(!vis[i])
-//             {
-//                 bool ans =dfs(i,vis,stack,adj);
-//                 if(ans)
-//                 {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                bool ans =dfs(i,vis,stack,adj);
+                if(ans)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     bool dfs(int src,vector<int> adj[],vector<bool> &vis,vector<bool> &stack)
     {
         vis[src]=1;
@@ -74,27 +72,27 @@ public:
         stack[src]=0;
         return false;
     }
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int> adj[n];
+//     bool canFinish(int n, vector<vector<int>>& pre) {
+//         vector<int> adj[n];
         
         
-        for(int i=0;i<pre.size();i++)
-        {
-            adj[pre[i][0]].push_back(pre[i][1]);
-        }
-        vector<bool> vis(n,0);
-        vector<bool> stack(n,0);
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                bool ans=dfs(i,adj,vis,stack);
-                if(ans)
-                {
-                    return false;;
-                }
-            }
-        }
-        return true;
-    }
+//         for(int i=0;i<pre.size();i++)
+//         {
+//             adj[pre[i][0]].push_back(pre[i][1]);
+//         }
+//         vector<bool> vis(n,0);
+//         vector<bool> stack(n,0);
+//         for(int i=0;i<n;i++)
+//         {
+//             if(!vis[i])
+//             {
+//                 bool ans=dfs(i,adj,vis,stack);
+//                 if(ans)
+//                 {
+//                     return false;;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
 };
