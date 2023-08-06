86f1175241d23396dd3d1c7838d6057ec238de8b
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-08-07 03:53:02 +0530

    Time: 3 ms (34.23%), Space: 44.8 MB (91.35%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,31 @@
 class Solution {
-    public static void dfs(int node,ArrayList<ArrayList<Integer>> adj ,int vis[] ){
+    public static void dfs(int node,int[] vis,ArrayList<ArrayList<Integer>> adj){
         vis[node]=1;
         for(Integer it:adj.get(node)){
             if(vis[it]==0){
-                dfs(it,adj,vis);
+                dfs(it,vis,adj);
             }
         }
-        
-        
     }
-    public int findCircleNum(int[][] adjmatrix) {
-        int n=adjmatrix.length;
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<ArrayList<Integer>>();
-        for(int i=0;i<n;i++){
+    public int findCircleNum(int[][] isconnected) {
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<isconnected.length;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(adjmatrix[i][j]==1 && i!=j){
+        for(int i=0;i<isconnected.length;i++){
+            for(int j=0;j<isconnected[i].length;j++){
+                if(isconnected[i][j]==1 && i!=j){
                     adj.get(i).add(j);
                     adj.get(j).add(i);
                 }
             }
         }
-        
-        int [] vis=new int[n];
         int count=0;
-        for(int i=0;i<n;i++){
+        int[] vis=new int[isconnected.length];
+        for(int i=0;i<isconnected.length;i++){
             if(vis[i]==0){
                 count++;
-                dfs(i,adj,vis);
+                dfs(i,vis,adj);
             }
         }
         return count;
