df63c809bfc4cf5986bfc6654e05558a10bb57ba
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-08-07 11:31:33 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (84.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,21 +1,27 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean bsR(int start,int end,int row,int[][] matrix,int target){
+        if(start>end)return false;
+        int mid=start+(end-start)/2;
         
-        int row=0;
-        for(int i=0;i<matrix.length;i++){
-            while(i<matrix.length && matrix[i][0]<target){
-                row=i;
-                i++;
-            }
-            if(i<matrix.length && matrix[i][0]==target){
-                return true;
-            }
+        if(target==matrix[row][mid])return true;
+        else if(target>matrix[row][mid]){
+            return bsR(mid+1,end,row,matrix,target);
         }
+        else if(target<matrix[row][mid]){
+            return bsR(start,mid-1,row,matrix,target);
+        }
+        return false;
+    }
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length-1;
         
-        for(int j=0;j<matrix[0].length;j++){
-            if(matrix[row][j]==target){
-                return true;
-            }
+        int start=0;
+        int end=m;
+        
+        for(int i=0;i<n;i++){
+            if(bsR(start,end,i,matrix,target)==true)return true;
         }
         return false;
     }
