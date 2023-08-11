7bcf4fb6546d24e5bccbed179fd3420bb2380ba8
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-08-11 15:32:52 +0530

    Time: 65 ms (30.85%), Space: 45.1 MB (7.13%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,140 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        String ans="";
+        int i=0,j=0;
+        int min=Integer.MAX_VALUE;
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(int k=0;k<t.length();k++)
+        {
+            char c=t.charAt(k);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        int count=map.size();
+        
+        while(j<s.length())
+        {
+            char c=s.charAt(j);
+            if(!map.containsKey(c))
+            {
+                j++;
+                continue;
+            }
+            else if(map.containsKey(c))
+            {
+                map.put(c,map.get(c)-1);
+                if(map.get(c)==0)
+                    count--;
+            }
+            if(count==0)
+            {
+                if(min>j-i+1)
+                {
+                    ans=s.substring(i,j+1);
+                    min=Math.min(min,j-i+1);
+                }
+                while(count==0)
+                {
+                    char c1=s.charAt(i);
+                    if(!map.containsKey(c1))
+                        i++;
+                    else{
+                        map.put(c1,map.get(c1)+1);
+                        if(map.get(c1)>0)
+                            count++;
+                        i++;
+                    }
+                    if(count==0)
+                    {
+                        if(min>j-i+1)
+                        {
+                            ans=s.substring(i,j+1);
+                            min=j-i+1;
+                        }
+                    }
+                }
+            }
+            j++;
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public String minWindow(String s, String t) {
+//         String ans="";
+//         int i=0;
+//         int j=0;
+//         int min=Integer.MAX_VALUE;
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         for(int q=0;q<t.length();q++){
+//             map.put(t.charAt(q),map.getOrDefault(t.charAt(q),0)+1);
+//         }
+//         int count=map.size();
+//         while(j<s.length()){
+//             if(!map.containsKey(s.charAt(j))){
+//                 j++;
+//                 continue;
+//             }
+//             else if(map.containsKey(s.charAt(j))){
+//                 map.put(s.charAt(j),map.get(s.charAt(j)-1));
+//                 if(map.get(s.charAt(j))==0)count--;
+//             }
+//             if(count==0){
+//                 if(min>j-i+1){
+//                     ans=s.substring(i,j+1);
+//                     min=Math.min(min,j-i+1);
+//                 }
+//                 while(count==0){
+//                     if(!map.containsKey(s.charAt(i)))
+//                         i++;
+//                     else{
+//                         map.put(s.charAt(i),map.get(s.charAt(i))+1);
+//                         if(map.get(s.charAt(i))>0)
+//                             count++;
+//                         i++;
+//                     }
+                    
+//                     if(count==0){
+//                         if(min>j-i+1){
+//                             ans=s.substring(i,j+1);
+//                             min=j-i+1;
+//                         }
+//                     }
+//                 }
+//             }
+//             j++;
+//         }
+//         return ans;
+//     }
+// }
