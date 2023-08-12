86937a129e41ea9aca1537d1bad028674f7f4f64
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-08-12 10:31:47 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (6.86%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,39 +1,20 @@
 class Solution {
-    public static int dfs(int row,int col,int n,int m,int[][] adj,int[][] dp){
-        if(row<0 || col<0 || row>n || col>m || adj[row][col]==1)return 0;
-        if(row==n && col==m)return 1;
+    public static int rec(int row,int col,int n,int m,int[][] obs,int[][] dp){
+        if(row<0 || row>n || col<0 || col>m || obs[row][col]==1)return 0;
+        if(row==n && col==m){
+            return 1;
+        }
         if(dp[row][col]!=-1)return dp[row][col];
-        int down=dfs(row+1,col,n,m,adj,dp);
-        int right=dfs(row,col+1,n,m,adj,dp);
+        int down = rec(row+1,col,n,m,obs,dp);
+        int right = rec(row,col+1,n,m,obs,dp);
         return dp[row][col]=down+right;
     }
-    public int uniquePathsWithObstacles(int[][] adj) {
-        int [][] dp=new int[adj.length][adj[0].length];
-        dp[0][0]=1;
-        for(int i=0;i<adj.length;i++){
-            for(int j=0;j<adj[0].length;j++){
-                if(adj[i][j]==1){
-                    dp[i][j]=0;
-                }
-                else if(i==0 && j==0){
-                    dp[i][j]=1;
-                    continue;
-                }
-                else{
-                     int down=0;
-                if(i>0){
-                     down=dp[i-1][j];
-                }
-                
-                int right=0;
-                if(j>0){
-                    right=dp[i][j-1];
-                }
-                dp[i][j]=down+right;
-                }
-               
-            }
+    public int uniquePathsWithObstacles(int[][] obs) {
+        int[][] dp=new int[obs.length+1][obs[0].length+1];
+        for(int[] it:dp){
+            Arrays.fill(it,-1);
         }
-        return dp[adj.length-1][adj[0].length-1];
+        return rec(0,0,obs.length-1,obs[0].length-1,obs,dp);
+        
     }
 }
