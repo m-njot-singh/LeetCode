86408f884608085fcd32c0887be2a95b4d2c9cad
Manjot singh <75523214+m-njot-singh@users.noreply.github.com>
2023-08-17 15:52:33 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (91.39%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,38 +1,30 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
-        // if(arr.length==2){
-        //     if(arr[0]>arr[1]){
-        //         return 0;
-        //     }
-        //     return 1;
-        // }
-        int idx=0;
-        for(int i=1;i<=arr.length-2;i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                // int count=1;
-                // int j=i;
-                // while(j>0 && arr[j]>arr[j-1]){
-                //         count++;
-                //         j--;
-                // }
-                idx=i;
-                // while(i<arr.length-1 && arr[i]>arr[i+1]){
-                //         count++;
-                //         i++;
-                // }
-            }
-            // else{
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1)return 0;
+        if(nums[0]>nums[1])return 0;
+        if(nums[nums.length-1]>nums[nums.length-2])return nums.length-1;
+        
+        int i=1;
+        int j=nums.length-2;
+        int ans=0;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            // if(nums[i]==nums[j]){
             //     i++;
+            //     j--;
             // }
-            
-        }
-        if(idx==0){
-            if(arr[0]>arr[arr.length-1]){
-                return 0;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                ans=mid;
+                break;
             }
-            return arr.length-1;
+            else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]){
+                i=mid+1;
+            }
+            else {
+                j=mid-1;
+            }
         }
         
-        return idx;
+        return ans;
     }
 }
